eeb9f65fcf0536231cc7bb53f5168738f6ab0a16
allan81 <rjaycondiman81@gmail.com>
2025-02-19 15:56:59 +0800

    Setup Backend Yii2 with creating migrations

---
+++ technical-test-backend/migrations/m250219_074919_create_users_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%users}}`.
+ */
+class m250219_074919_create_users_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%users}}', [
+            'id' => $this->primaryKey(),
+            'name' => $this->string(255)->notNull(),
+            'birthday' => $this->date()->notNull(),
+            'created_at' => $this->timestamp()->defaultExpression('CURRENT_TIMESTAMP')
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('{{%users}}');
+    }
+}
